39862ea7559c923dc0b3fd02bac14990accd018a
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2020-04-29 16:20:58 +0200

    Update exoscale/client.go
    Co-Authored-By: Marc Falzon <falzm@users.noreply.github.com>

--- exoscale/client.go
+++ exoscale/client.go
@@ -40,7 +40,7 @@ func newExoscaleClient() (*egoscale.Client, error) {
 		return nil, fmt.Errorf("configuration missing for API Key %q or Secret Key %q", envKey, envSecret)
 	}
 
-	egoscale.UserAgent = fmt.Sprintf("Exoscale-CCM/%s %s", versionString, egoscale.UserAgent)
+	egoscale.UserAgent = fmt.Sprintf("Exoscale-K8s-Cloud-Controller/%s %s", versionString, egoscale.UserAgent)
 
 	return egoscale.NewClient(envEndpoint, envKey, envSecret), nil
 }
