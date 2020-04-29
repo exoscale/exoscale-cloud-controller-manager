1dd4ee6a23e1c992765a95a117009dbfdc21ef4d
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2020-04-29 16:26:32 +0200

    fix
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- exoscale/client.go
+++ exoscale/client.go
@@ -13,24 +13,15 @@ const defaultComputeEndpoint = "https://api.exoscale.com/v1"
 func newExoscaleClient() (*egoscale.Client, error) {
 	// an attempt to mimic existing behaviours
 	envEndpoint := readFromEnv(
-		"EXOSCALE_ENDPOINT",
-		"EXOSCALE_COMPUTE_ENDPOINT",
-		"CLOUDSTACK_ENDPOINT",
+		"EXOSCALE_API_ENDPOINT",
 	)
 
 	envKey := readFromEnv(
-		"EXOSCALE_KEY",
 		"EXOSCALE_API_KEY",
-		"CLOUDSTACK_KEY",
-		"CLOUSTACK_API_KEY",
 	)
 
 	envSecret := readFromEnv(
-		"EXOSCALE_SECRET",
 		"EXOSCALE_API_SECRET",
-		"EXOSCALE_SECRET_KEY",
-		"CLOUDSTACK_SECRET",
-		"CLOUDSTACK_SECRET_KEY",
 	)
 
 	if envEndpoint == "" {
