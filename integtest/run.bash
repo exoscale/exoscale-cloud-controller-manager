2c27609f4d59f54149cd7372a1721890a05cfbab
Johnatan Illi <johnatan.illi@exoscale.ch>
2020-07-06 10:44:03 +0200

    fixup! fix review

--- integtest/run.bash
+++ integtest/run.bash
@@ -65,7 +65,7 @@ remote_run() {
 rsync -a "$INCLUDE_PATH/" "ubuntu@$EXOSCALE_MASTER_IP:/home/ubuntu" \
       -e "ssh -o StrictHostKeyChecking=no -i $INTEGTEST_DIR/.ssh/id_rsa"
 
-remote_run "sudo kubeadm init --config=$INTEGTEST_DIR/manifests/kubeadm-config-master.yml"
+remote_run "sudo kubeadm init --config=./integtest/manifests/kubeadm-config-master.yml"
 remote_run "sudo cp -f /etc/kubernetes/admin.conf admin.conf && sudo chown ubuntu:ubuntu admin.conf"
 
 mkdir -p "$INTEGTEST_DIR/.kube"
