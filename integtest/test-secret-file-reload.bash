1c514a5946f2095a02011fb07ed914541ff01170
Antoine Baudrand <antoine.baudrand@exoscale.ch>
2021-02-03 19:33:30 +0100

    fixup! Add integration tests

--- integtest/test-secret-file-reload.bash
+++ integtest/test-secret-file-reload.bash
@@ -12,7 +12,7 @@ CCM_POD="$(kubectl get pods -n kube-system -l app=exoscale-cloud-controller-mana
 _until_success "kubectl -n kube-system logs \"$CCM_POD\" | grep -m 1 \"Exoscale API credentials refreshed, now using test\""
 
 IAM_SECRET_JSON="{\"name\":\"good\",\"api_key\":\"$EXOSCALE_API_KEY\",\"api_secret\":\"$EXOSCALE_API_SECRET\"}"
-kubectl exec -ti -n kube-system "$CCM_POD" -- env IAM_SECRET_JSON="$IAM_SECRET_JSON" ash -c 'echo $IAM_SECRET_JSON > /tmp/iam-keys'
+kubectl exec -n kube-system "$CCM_POD" -- env IAM_SECRET_JSON="$IAM_SECRET_JSON" ash -c 'echo $IAM_SECRET_JSON > /tmp/iam-keys'
 
 _until_success "kubectl -n kube-system logs \"$CCM_POD\" | grep -m 1 \"Exoscale API credentials refreshed, now using good\""
 
