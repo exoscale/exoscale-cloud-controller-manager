fb040af22e76070aa961321bddafe81b3298190f
Predrag Janosevic <predrag.janosevic@exoscale.com>
2024-05-16 14:12:34 +0100

    Update examples in docs/

--- docs/scripts/generate-secret.sh
+++ docs/scripts/generate-secret.sh
@@ -8,7 +8,7 @@ metadata:
   namespace: kube-system
 type: Opaque
 data:
-  api-endpoint: '$(printf "%s" "$EXOSCALE_API_ENDPOINT" | base64)'
   api-key: '$(printf "%s" "$EXOSCALE_API_KEY" | base64)'
   api-secret: '$(printf "%s" "$EXOSCALE_API_SECRET" | base64)'
+  api-zone: '$(printf "%s" "$EXOSCALE_ZONE" | base64)'
 EOF
