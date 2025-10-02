01c8c1c7d881111146b36da94f97e580839c9a29
Philippe Chepy <philippe.chepy@exoscale.ch>
2025-10-02 19:31:33 +0200

    Remove legacy testing with pytest

--- test/terraform/sks/control-plane/helpers.tf
+++
@@ -1,8 +0,0 @@
-## Helper resources
-
-# Unique test ID (suffix)
-resource "random_string" "test_id" {
-  length  = 5
-  upper   = false
-  special = false
-}
--- test/terraform/sks/nodes/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  ## Test parameters
-
-  # Unique test ID/name
-  test_name = "${var.test_name}-${var.test_id}"
-}
